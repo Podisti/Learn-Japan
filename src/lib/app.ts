5e22f3bc2a62c45e80f9545f0a230d66c722c675
Podisti <ondra.beni@gmail.com>
2022-03-20 10:35:14 +0100

    Functional version

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -2,28 +2,33 @@ function test(): string {
     return new Date().toDateString();
 }
 
-function getRandomInt(min, max) : number{ //generátor náhodného čísla
+function getRandomInt(min, max): number { //generátor náhodného čísla
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; 
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function displayKata() { // zobrazí znak
     let letters: string[] = ["ア", "イ", "ウ", "エ", "オ", "カ", "キ", "ク", "ケ", "コ", "キャ", "キュ", "キョ", "サ",
-                            "シ", "ス", "セ", "ソ", "シャ", "シュ", "ショ", "タ", "チ", "ツ", "テ", "ト", "チャ", "チュ",
-                            "チョ", "ナ", "ニ", "ヌ", "ネ", "ノ", "ニャ", "ニュ", "ニョ", "ハ", "ヒ", "フ", "ヘ", "ホ",
-                            "ヒャ", "ヒュ", "ヒョ", "マ", "ミ", "ム", "メ", "モ", "ミャ", "ミュ", "ミョ", "ヤ", "ユ", "ヨ",
-                            "ラ", "リ", "ル", "レ", "ロ"/*, "リャ", "リュ", "リョ", "ワ", "ン", "ガ", "ギ", "グ", "ゲ", "ゴ",
-                            "ギャ", "ギュ", "ギョ", "ザ", "ジ", "ズ", "ゼ", "ゾ", "ジャ", "ジュ", "ジョ", "ダ", "ヂ", "バ",
-                            "ビ", "ブ", "ベ", "ボ", "ビャ", "ビュ", "ビョ", "パ", "ピ", "プ", "ペ", "ポ", "ピャ", "ピュ",
-"ピョ", "ファ", "フィ", "フェ", "フォ", "ツァ", "ティ", "トゥ", "ウェ", "ウォ"*/];
+        "シ", "ス", "セ", "ソ", "シャ", "シュ", "ショ", "タ", "チ", "ツ", "テ", "ト", "チャ", "チュ",
+        "チョ", "ナ", "ニ", "ヌ", "ネ", "ノ", "ニャ", "ニュ", "ニョ", "ハ", "ヒ", "フ", "ヘ", "ホ",
+        "ヒャ", "ヒュ", "ヒョ", "マ", "ミ", "ム", "メ", "モ", "ミャ", "ミュ", "ミョ", "ヤ", "ユ", "ヨ",
+        "ラ", "リ", "ル", "レ", "ロ", "リャ", "リュ", "リョ", "ワ", "ン", "ガ", "ギ", "グ", "ゲ", "ゴ",
+        "ギャ", "ギュ", "ギョ", "ザ", "ジ", "ズ", "ゼ", "ゾ", "ジャ", "ジュ", "ジョ", "ダ", "ヂ", "バ",
+        "ビ", "ブ", "ベ", "ボ", "ビャ", "ビュ", "ビョ", "パ", "ピ", "プ", "ペ", "ポ", "ピャ", "ピュ",
+        "ピョ", "ファ", "フィ", "フェ", "フォ", "ツァ", "ティ", "トゥ", "ウェ", "ウォ"];
 
     var chosen = letters[getRandomInt(0, letters.length)]
 
     document.getElementById("display").innerText = chosen;
+    document.getElementById("display").style.background = "rgb(179, 171, 248)";
+    (<HTMLInputElement>document.getElementById("answer")).value = "";
+
+    (<HTMLInputElement>document.getElementById("button")).setAttribute("onClick", "javascript: checkKata();");
+    (<HTMLInputElement>document.getElementById("button")).innerText = "check";
 }
 
-function checkKata(){
+function checkKata() {
 
     let table: { [key: string]: string } = {
         "ア": "a", "イ": "i", "ウ": "u", "エ": "e", "オ": "o",
@@ -37,19 +42,23 @@ function checkKata(){
         "ラ": "ra", "リ": "ri", "ル": "ru", "レ": "re", "ロ": "ro", "リャ": "rya", "リュ": "ryu", "リョ": "ryo",
         "ワ": "wa",
         "ン": "n",
-        /*"ガ": "ga", "ギ": "gi", "グ": "gu", "ゲ": "ge", "ゴ": "go", "ギャ": "gya", "ギュ": "guy", "ギョ": "gyo",
+        "ガ": "ga", "ギ": "gi", "グ": "gu", "ゲ": "ge", "ゴ": "go", "ギャ": "gya", "ギュ": "guy", "ギョ": "gyo",
         "ザ": "za", "ジ": "ji", "ズ": "zu", "ゼ": "ze", "ゾ": "zo", "ジャ": "ja", "ジュ": "ju", "ジョ": "jo",
         "ダ": "da", "ヂ": "ji", "ヅ": "zu", "デ": "de", "ド": "do",
         "バ": "ba", "ビ": "bi", "ブ": "bu", "ベ": "be", "ボ": "bo", "ビャ": "bya", "ビュ": "byu", "ビョ": "byo",
         "パ": "pa", "ピ": "pi", "プ": "pu", "ペ": "pe", "ポ": "po", "ピャ": "pya", "ピュ": "pyu", "ピョ": "pyo",
         "ファ": "fa", "フィ": "fi", "フェ": "fe", "フォ": "fo",
-        "ツァ": "tsa", "ティ": "ti", "トゥ": "tu", "ウェ": "we", "ウォ": "wo"*/
+        "ツァ": "tsa", "ティ": "ti", "トゥ": "tu", "ウェ": "we", "ウォ": "wo"
     }
 
     if (((<HTMLInputElement>document.getElementById("answer")).value) == table[document.getElementById("display").innerText]) {
         (<HTMLInputElement>document.getElementById("answer")).value = "correct";
+        document.getElementById("display").style.background = "lightgreen";
     }
     else {
         (<HTMLInputElement>document.getElementById("answer")).value = table[document.getElementById("display").innerText];
+        document.getElementById("display").style.background = "red";
     }
+    (<HTMLInputElement>document.getElementById("button")).setAttribute("onClick", "javascript: displayKata();");
+    (<HTMLInputElement>document.getElementById("button")).innerText = "next";
 }
